8f94d206fbfddf5f19abf80662c57a88df28ca2e
Lian Dynn <lian_dynn@pm.me>
2025-11-02 14:25:59 -0400

    Trying disk encryption now

--- hosts/workpad/disko.nix
+++ hosts/workpad/disko.nix
@@ -1,55 +1,48 @@
 {
   disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
+    disk.main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            priority = 1;
+            name = "ESP";
+            start = "1M";
+            end = "2G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
-            root = {
-              size = "100%";
-              type = "8300";
+          };
+
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "cryptroot";
+              settings = {
+                allowDiscards = true;
+                keyFile = null;
+              };
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ];
                 subvolumes = {
                   "@" = {
-                    mountOptions = [
-                      "ssd"
-                      "noatime"
-                      "discard=async"
-                      "compress=zstd"
-                    ];
+                    mountOptions = [ "ssd" "noatime" "discard=async" "compress=zstd" ];
                     mountpoint = "/";
                   };
                   "@/home" = {
-                    mountOptions = [
-                      "ssd"
-                      "noatime"
-                      "discard=async"
-                      "compress=zstd"
-                    ];
+                    mountOptions = [ "ssd" "noatime" "discard=async" "compress=zstd" ];
                     mountpoint = "/home";
                   };
                   "@/nix" = {
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
+                    mountOptions = [ "compress=zstd" "noatime" ];
                     mountpoint = "/nix";
                   };
                   "@/swap" = {

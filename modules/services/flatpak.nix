48c18bff4f13f65e8d726b38e1cfa73361563c42
Lian Dynn <lian_dynn@pm.me>
2025-11-02 04:56:29 -0400

    Fixed portals

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -1,11 +1,6 @@
 { pkgs, ... }: {
   services.flatpak.enable = true;
   xdg = {
-    enable = true;
-    mime.enable = true;
-    mimeApps = {
-      enable = true;
-    };
     portal = {
       enable = true;
       extraPortals = [ pkgs.xdg-desktop-portal-gtk ];

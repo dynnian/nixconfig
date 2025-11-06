3709a93f3cb33f50814e7021722a19c956c2c1c7
Lian Dynn <lian_dynn@pm.me>
2025-11-06 07:57:49 -0400

    Cleaned up some things

---
+++ home/config/default (conflicted copy 2025-11-05 233043).nix
@@ -0,0 +1,27 @@
+{ ... }: {
+  imports = [
+    ./btop.nix
+    ./fish.nix
+    ./fuzzel.nix
+    ./zathura.nix
+    ./git.nix
+    ./kanshi.nix
+    ./kitty.nix
+    ./nvim.nix
+    ./newsboat.nix
+    ./starship.nix
+    ./swayosd.nix
+    ./qt.nix
+    ./gtk.nix
+    ./virtmanager.nix
+    ./waybar.nix
+    ./xdg.nix
+    ./mimeApps.nix
+    ./mpv.nix
+    ./swaync.nix
+    ./bash
+    ./hyprland
+    ./scripts
+    ./yazi
+  ];
+}
--- home/config/scripts/default.nix
+++ home/config/scripts/default.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }: 
-{
+{ pkgs, ... }: {
   home.packages = with pkgs; [
     # rs_blue
     (writeShellScriptBin

e2b80be442dcb61dbe6558dd5ff860ff186e0496
Lian Dynn <lian_dynn@pm.me>
2025-11-12 13:42:36 -0400

    Fixed pipewire

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -11,21 +11,6 @@
     };
     pulse.enable = true;
     jack.enable = true;
-    wireplumber = {
-      enable = true;
-      extraConfig."10-bluez" = {
-        "monitor.bluez.properties" = {
-          "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = true;
-          "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [
-            "hsp_hs"
-            "hsp_ag"
-            "hfp_hf"
-            "hfp_ag"
-          ];
-        };
-      };
-    };
+    wireplumber.enable = true;
   };
 }

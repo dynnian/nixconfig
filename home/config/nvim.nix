9b24ed1705f05d16b7bb77edf21d817d778e3087
Lian Dynn <lian_dynn@pm.me>
2025-12-11 11:00:25 -0400

    Trying to add C# LSP to neovim

--- home/config/nvim.nix
+++ home/config/nvim.nix
@@ -24,7 +24,7 @@
       };
     };
 
-    # Plugins
+    # Plugins (Nixvim modules)
     plugins = {
       # Neo-tree file browser (modern replacement for NERDTree)
       neo-tree = {
@@ -49,14 +49,20 @@
     extraPlugins = with pkgs.vimPlugins; [
       vim-nix
       vim-javascript
-      # CoC and extensions
-      coc-nvim
-      coc-go
-      coc-css
-      coc-emmet
-      coc-html
-      coc-json
-      coc-csharp-ls
+
+      # Native LSP + auto-install stack
+      nvim-lspconfig
+      mason-nvim
+      mason-lspconfig-nvim
+
+      # Completion + snippets
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      LuaSnip
+      cmp_luasnip
+      friendly-snippets
 
       # Terminal emulator
       toggleterm-nvim
@@ -127,7 +133,101 @@
 
     # Additional Lua configuration
     extraConfigLua = ''
+      -----------------------------------------------------------------------
+      -- Mason: manage LSP servers (automatic download, like LunarVim)
+      -----------------------------------------------------------------------
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+
+      mason.setup()
+
+      mason_lspconfig.setup({
+        ensure_installed = {
+          "csharp_ls",   -- C#
+          "jsonls",
+          "html",
+          "cssls",
+          "lua_ls"
+        },
+      })
+
+      -----------------------------------------------------------------------
+      -- nvim-cmp + LuaSnip: completion + snippets
+      -----------------------------------------------------------------------
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      -- Load VSCode-style snippets (from friendly-snippets)
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+      -----------------------------------------------------------------------
+      -- LSP setup: capabilities + on_attach + automatic server setup
+      -----------------------------------------------------------------------
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local on_attach = function(_, bufnr)
+        local bufmap = function(mode, lhs, rhs)
+          vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, buffer = bufnr })
+        end
+
+        bufmap("n", "K", vim.lsp.buf.hover)
+        bufmap("n", "gd", vim.lsp.buf.definition)
+        bufmap("n", "gr", vim.lsp.buf.references)
+        bufmap("n", "gi", vim.lsp.buf.implementation)
+        bufmap("n", "<leader>rn", vim.lsp.buf.rename)
+        bufmap("n", "<leader>ca", vim.lsp.buf.code_action)
+      end
+
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+          })
+        end,
+      })
+
+      -----------------------------------------------------------------------
       -- Transparent background highlights
+      -----------------------------------------------------------------------
       vim.cmd([[
         hi Normal      guibg=NONE ctermbg=NONE
         hi LineNr      guibg=NONE ctermbg=NONE
@@ -136,7 +236,9 @@
         hi Visual      cterm=none ctermbg=darkgrey ctermfg=white
       ]])
 
+      -----------------------------------------------------------------------
       -- toggleterm setup
+      -----------------------------------------------------------------------
       require("toggleterm").setup({
         size = 14,
         hide_numbers = true,

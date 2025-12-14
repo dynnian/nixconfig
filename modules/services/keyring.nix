91e30e1fff47c5cb49d638841d3e30096393fdb0
Lian Dynn <lian_dynn@pm.me>
2025-12-14 16:45:21 -0400

    Testing

--- modules/services/keyring.nix
+++ modules/services/keyring.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   services.gnome.gnome-keyring.enable = true;
   security.pam.services = {
+    ly.enableGnomeKeyring = true;
     login.enableGnomeKeyring = true;
   };
   services.dbus.packages = [ pkgs.gcr ];

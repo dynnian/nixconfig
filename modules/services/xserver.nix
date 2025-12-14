95b5dc0c39b0734a3a4adc79d8ea11c61666c510
Lian Dynn <lian_dynn@pm.me>
2025-12-14 15:48:44 -0400

    Env vars to force stuff

--- modules/services/xserver.nix
+++ modules/services/xserver.nix
@@ -1,4 +1,15 @@
 { pkgs, ... }: {
+  environment.sessionVariables = {
+    MOZ_USE_XINPUT2 = "1";
+    QT_AUT_SCREEN_SCALE_FACTOR = "1";
+    XKB_DEFAULT_LAYOUT = "us";
+    XDG_SESSION_TYPE = "x11";
+    XDG_CURRENT_DESKTOP = "GNOME";
+    XDG_SESSION_DESKTOP = "GNOME";
+    XDG_MENU_PREFIX = "gnome-";
+    WINDOW_MANAGER = "dwm";
+  };
+
   services.xserver = {
     enable = true;
 
@@ -6,9 +17,11 @@
       enable = true;
       package = pkgs.awesome;
     };
+
     displayManager.sessionCommands = ''
       eval $(gnome-keyring-daemon --start --daemonize --components=ssh,secrets)
-      export SSH_AUTH_SOCK '';
+      export SSH_AUTH_SOCK
+    '';
   };
 
   services.displayManager.sddm = {

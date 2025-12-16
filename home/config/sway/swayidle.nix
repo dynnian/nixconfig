55336a426a664c0e669cd4b5400742649ef5b7ef
Lian Dynn <lian_dynn@pm.me>
2025-12-16 01:43:47 -0400

    Fixed this

--- home/config/sway/swayidle.nix
+++ home/config/sway/swayidle.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 let
-  lockcmd = ''
-    swaylock -f -i "$(ls -1 "$HOME/.config/sway/wallpaper/locked."* 2>/dev/null | head -n1)"
-  '';
+  lockcmd = "swaylock -f -i \"$(ls -1 \"$HOME/.config/sway/wallpaper/locked.\"{jpg,png} 2>/dev/null | head -n1)\"";
   screenoff = "swaymsg 'output * power off'";
   screenon  = "swaymsg 'output * power on'";
 in {
@@ -13,12 +11,14 @@ in {
     extraArgs = [ "-w" ];
 
     events = [
-      { event = "lock";        command = lockcmd; }
+      { event = "lock";         command = lockcmd; }
       { event = "before-sleep"; command = lockcmd; }
     ];
 
     timeouts = [
+      # Lock screen after 5 minutes (300 seconds)
       { timeout = 300; command = lockcmd; }
+      # Turn off screen after 10 minutes (600 seconds), turn it back on upon resume
       { timeout = 600; command = screenoff; resumeCommand = screenon; }
     ];
   };

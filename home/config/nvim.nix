5d6dfc25ddd117687ad9087eb57a5e09bf98ea4e
Lian Dynn <lian_dynn@pm.me>
2025-12-23 15:39:01 -0400

    Added some LSPs

--- home/config/nvim.nix
+++ home/config/nvim.nix
@@ -62,15 +62,16 @@
       lsp = {
         enable = true;
         servers = {
-           bashls.enable = true;
-           nixd.enable   = true;
-           lua_ls.enable = true;
-           csharp_ls.enable = true;
-           gopls.enable = true;
-           rust_analyzer = { 
-             enable = true; 
-             installCargo = false;
-           };
+          bashls.enable = true;
+          nixd.enable   = true;
+          lua_ls.enable = true;
+          csharp_ls.enable = true;
+          gopls.enable = true;
+          rust_analyzer = { 
+            enable = true; 
+            installCargo = false;
+            installRustc = false;
+          };
         };
       };
 

af73dfb0974607793102f1d77a3128b32ee99538
Lian Dynn <lian_dynn@pm.me>
2025-12-23 16:55:28 -0400

    Added fast window movement

--- home/config/nvim/keymaps.nix
+++ home/config/nvim/keymaps.nix
@@ -27,6 +27,32 @@
         action = "<C-w>l";
         options = { silent = true; desc = "Window right"; };
       }
+
+      # Fast ones with just ctrl
+      {
+        mode = "n";
+        key = "<C-h>";
+        action = "<C-w>h";
+        options = { silent = true; desc = "Window left"; };
+      }
+      {
+        mode = "n";
+        key = "<C-j>";
+        action = "<C-w>j";
+        options = { silent = true; desc = "Window down"; };
+      }
+      {
+        mode = "n";
+        key = "<C-k>";
+        action = "<C-w>k";
+        options = { silent = true; desc = "Window up"; };
+      }
+      {
+        mode = "n";
+        key = "<C-l>";
+        action = "<C-w>l";
+        options = { silent = true; desc = "Window right"; };
+      }
       
       # Splits
       {

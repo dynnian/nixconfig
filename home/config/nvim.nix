3ca2b82bd1b523ac1454159408aebf50168506a5
Lian Dynn <lian_dynn@pm.me>
2025-12-23 16:05:42 -0400

    Improved nvim

--- home/config/nvim.nix
+++ home/config/nvim.nix
@@ -42,6 +42,8 @@
 
     # Plugins
     plugins = {
+      gitsings.enable = true;
+      neogit.enable = true;
       neo-tree = {
         enable = true;
         settings = {
@@ -337,6 +339,205 @@
         options = { silent = true; desc = "Only window"; };
       }
 
+      ## LSP (Doom-ish: <leader>l ...)
+      # Jump / navigation (classic)
+      {
+        mode = "n";
+        key = "gd";
+        action = "<cmd>lua vim.lsp.buf.definition()<CR>";
+        options = { silent = true; desc = "LSP: go to definition"; };
+      }
+      {
+        mode = "n";
+        key = "gD";
+        action = "<cmd>lua vim.lsp.buf.declaration()<CR>";
+        options = { silent = true; desc = "LSP: go to declaration"; };
+      }
+      {
+        mode = "n";
+        key = "gI";
+        action = "<cmd>lua vim.lsp.buf.implementation()<CR>";
+        options = { silent = true; desc = "LSP: go to implementation"; };
+      }
+      {
+        mode = "n";
+        key = "gr";
+        action = "<cmd>lua vim.lsp.buf.references()<CR>";
+        options = { silent = true; desc = "LSP: references"; };
+      }
+      {
+        mode = "n";
+        key = "K";
+        action = "<cmd>lua vim.lsp.buf.hover()<CR>";
+        options = { silent = true; desc = "LSP: hover"; };
+      }
+      {
+        mode = "n";
+        key = "<C-k>";
+        action = "<cmd>lua vim.lsp.buf.signature_help()<CR>";
+        options = { silent = true; desc = "LSP: signature help"; };
+      }
+      
+      # Leader LSP actions
+      {
+        mode = "n";
+        key = "<leader>la";
+        action = "<cmd>lua vim.lsp.buf.code_action()<CR>";
+        options = { silent = true; desc = "LSP: code action"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>lr";
+        action = "<cmd>lua vim.lsp.buf.rename()<CR>";
+        options = { silent = true; desc = "LSP: rename"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>lf";
+        action = "<cmd>lua vim.lsp.buf.format({ async = true })<CR>";
+        options = { silent = true; desc = "LSP: format"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>ld";
+        action = "<cmd>lua vim.diagnostic.open_float()<CR>";
+        options = { silent = true; desc = "Diagnostics: line"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>lq";
+        action = "<cmd>lua vim.diagnostic.setloclist()<CR>";
+        options = { silent = true; desc = "Diagnostics: loclist"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>lQ";
+        action = "<cmd>lua vim.diagnostic.setqflist()<CR>";
+        options = { silent = true; desc = "Diagnostics: quickfix"; };
+      }
+      
+      # Diagnostics navigation
+      {
+        mode = "n";
+        key = "[d";
+        action = "<cmd>lua vim.diagnostic.goto_prev()<CR>";
+        options = { silent = true; desc = "Diagnostics: previous"; };
+      }
+      {
+        mode = "n";
+        key = "]d";
+        action = "<cmd>lua vim.diagnostic.goto_next()<CR>";
+        options = { silent = true; desc = "Diagnostics: next"; };
+      }
+      
+      # Symbols (use built-in LSP, you already have Telescope ones too)
+      {
+        mode = "n";
+        key = "<leader>ls";
+        action = "<cmd>lua vim.lsp.buf.document_symbol()<CR>";
+        options = { silent = true; desc = "LSP: document symbols"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>lS";
+        action = "<cmd>lua vim.lsp.buf.workspace_symbol('')<CR>";
+        options = { silent = true; desc = "LSP: workspace symbols"; };
+      }
+      
+      # LSP info / restart
+      {
+        mode = "n";
+        key = "<leader>li";
+        action = "<cmd>LspInfo<CR>";
+        options = { silent = true; desc = "LSP: info"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>lR";
+        action = "<cmd>LspRestart<CR>";
+        options = { silent = true; desc = "LSP: restart"; };
+      }
+
+      ## Git (SPC g ...)
+      {
+        mode = "n";
+        key = "<leader>gg";
+        action = ":Neogit<CR>";
+        options = { silent = true; desc = "Git: status (Neogit)"; };
+      }
+
+      # Commit (open Neogit commit popup)
+      {
+        mode = "n";
+        key = "<leader>gc";
+        action = "<cmd>lua require('neogit').open({ kind = 'tab' }); vim.cmd('Neogit commit')<CR>";
+        options = { silent = true; desc = "Git: commit"; };
+      }
+      
+      # Push (open Neogit push popup)
+      {
+        mode = "n";
+        key = "<leader>gp";
+        action = "<cmd>lua require('neogit').open({ kind = 'tab' }); vim.cmd('Neogit push')<CR>";
+        options = { silent = true; desc = "Git: push"; };
+      }
+      
+      # Hunks
+      {
+        mode = "n";
+        key = "<leader>gh";
+        action = ":Gitsigns preview_hunk<CR>";
+        options = { silent = true; desc = "Preview hunk"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>gs";
+        action = ":Gitsigns stage_hunk<CR>";
+        options = { silent = true; desc = "Stage hunk"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>gr";
+        action = ":Gitsigns reset_hunk<CR>";
+        options = { silent = true; desc = "Reset hunk"; };
+      }
+      
+      # File-level
+      {
+        mode = "n";
+        key = "<leader>gS";
+        action = ":Gitsigns stage_buffer<CR>";
+        options = { silent = true; desc = "Stage buffer"; };
+      }
+      {
+        mode = "n";
+        key = "<leader>gR";
+        action = ":Gitsigns reset_buffer<CR>";
+        options = { silent = true; desc = "Reset buffer"; };
+      }
+      
+      # Navigation
+      {
+        mode = "n";
+        key = "]c";
+        action = ":Gitsigns next_hunk<CR>";
+        options = { silent = true; desc = "Next hunk"; };
+      }
+      {
+        mode = "n";
+        key = "[c";
+        action = ":Gitsigns prev_hunk<CR>";
+        options = { silent = true; desc = "Previous hunk"; };
+      }
+      
+      # Blame
+      {
+        mode = "n";
+        key = "<leader>gb";
+        action = ":Gitsigns blame_line<CR>";
+        options = { silent = true; desc = "Blame line"; };
+      }
+
       ## Neotree
       {
         mode = "n";
@@ -487,6 +688,25 @@
         direction = "float",
         float_opts = { border = "rounded", winblend = 0 },
       })
+
+      --------------------------------------------------
+      -- which key
+      --------------------------------------------------
+      pcall(function()
+        local wk = require("which-key")
+        wk.add({
+          { "<leader>b", group = "Buffers" },
+          { "<leader>e", group = "Explorer" },
+          { "<leader>f", group = "Find" },
+          { "<leader>g", group = "Git" },
+          { "<leader>l", group = "LSP" },
+          { "<leader>o", group = "Open" },
+          { "<leader>q", group = "Quit/Session" },
+          { "<leader>t", group = "Terminal" },
+          { "<leader>w", group = "Windows" },
+          { "<leader>x", group = "Diagnostics" },
+        })
+      end)
 
       --------------------------------------------------
       -- nvim-cmp + LuaSnip setup

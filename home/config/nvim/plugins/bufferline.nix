e79323226fcbad759097c8e49118293efcb23dbc
Lian Dynn <lian_dynn@pm.me>
2025-12-24 11:12:23 -0400

    That didn't work

--- home/config/nvim/plugins/bufferline.nix
+++ home/config/nvim/plugins/bufferline.nix
@@ -9,34 +9,6 @@
         show_close_icon = false;
         show_buffer_close_icons = false;
       };
-      highlights = {
-        fill.bg = "NONE";
-        background.bg = "NONE";
-
-        buffer_visible = {
-          bg = "NONE";
-        };
-        buffer_selected = {
-          bg = "NONE";
-        };
-
-        tab.bg = "NONE";
-        tab_selected.bg = "NONE";
-        tab_separator.bg = "NONE";
-        tab_separator_selected.bg = "NONE";
-
-        separator.bg = "NONE";
-        separator_visible.bg = "NONE";
-        separator_selected.bg = "NONE";
-
-        close_button.bg = "NONE";
-        close_button_visible.bg = "NONE";
-        close_button_selected.bg = "NONE";
-
-        modified.bg = "NONE";
-        modified_visible.bg = "NONE";
-        modified_selected.bg = "NONE";
-      };
     };
   };
 }

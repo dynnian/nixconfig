b60f1c40478751548689553acedf51b9af1c2c7a
Lian Dynn <lian_dynn@pm.me>
2026-01-05 18:46:26 -0400

    Issue I just realized

--- home/config/mpv.nix
+++ home/config/mpv.nix
@@ -2,11 +2,11 @@
   programs.mpv = {
     enable = true;
     extraInput = ''
-      "l seek 5"
-      "h seek -5"
-      "j seek -60"
-      "k seek 60"
-      "S cycle sub"
+      l seek 5
+      h seek -5
+      j seek -60
+      k seek 60
+      S cycle sub
     '';
   };
 }

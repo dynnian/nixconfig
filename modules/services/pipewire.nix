30a535772df9a40d9237f0267f437f3321200ce3
Lian Dynn <lian_dynn@pm.me>
2026-01-07 10:53:57 -0400

    Bruh

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -1,19 +1,28 @@
-{ ... }: {
+{ hostname, lib, ... }: {
   security.rtkit.enable = true;
 
   services.pipewire = {
     enable = true;
+
     alsa = {
       enable = true;
       support32Bit = true;
     };
+
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
-    extraConfig = {
+
+    extraConfig = lib.mkIf (hostname == "zweihander") {
       pipewire."10-custom-formats.conf" = {
         "context.properties" = {
-          "default.clock.allowed-rates" = [ 44100 48000 88200 96000 192000 ];
+          "default.clock.allowed-rates" = [
+            44100
+            48000
+            88200
+            96000
+            192000
+          ];
         };
       };
     };

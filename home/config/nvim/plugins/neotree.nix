7947136b637bab984f3c166d0f06fd2b64a05c30
Lian Dynn <lian_dynn@pm.me>
2026-01-07 11:17:10 -0400

    This option is messy

--- home/config/nvim/plugins/neotree.nix
+++ home/config/nvim/plugins/neotree.nix
@@ -2,7 +2,6 @@
   programs.nixvim.plugins.neo-tree = {
     enable = true;
     settings = {
-      close_if_last_window = true;
       window = { width = 30; };
       filesystem = {
         filtered_items = {

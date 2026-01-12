1a35861a5c660be5b57b861aa06196e711f633bf
Lian Dynn <lian_dynn@pm.me>
2026-01-12 12:18:31 -0400

    So XDG portals were wrong all along???

--- home/config/xdg.nix
+++ home/config/xdg.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }: {
   xdg.portal = {
     enable = true;
+    wlr.enable = true;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
     ];
 
     # Make sure sway gets a portals config generated:

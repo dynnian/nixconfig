adbb7b5c667e162d08ce79209c8e558661ed7638
Lian Dynn <lian_dynn@pm.me>
2026-01-18 03:27:13 -0400

    Qutebrowser adjusted

--- home/config/qutebrowser.nix
+++ home/config/qutebrowser.nix
@@ -1,7 +1,8 @@
 { ... }: let
+  theme = import ./../../user/theme.nix {};
   # --- Gruvbox Dark Hard Palette ---
-#  bg0_hard = "#1d2021";
-#  bg0_soft = "#32302f";
+  # bg0_hard = "#1d2021";
+  # bg0_soft = "#32302f";
   bg0_normal = "#282828";
 
   bg0 = bg0_normal;
@@ -22,20 +23,50 @@
   bright_blue = "#83a598";
   bright_purple = "#d3869b";
   bright_aqua = "#8ec07c";
-#  bright_gray = "#928374";
+  # bright_gray = "#928374";
   bright_orange = "#fe8019";
 
   dark_red = "#cc241d";
-#  dark_green = "#98971a";
-#  dark_yellow = "#d79921";
+  # dark_green = "#98971a";
+  # dark_yellow = "#d79921";
   dark_blue = "#458588";
   dark_purple = "#b16286";
   dark_aqua = "#689d6a";
-#  dark_gray = "#a89984";
-#  dark_orange = "#d65d0e";
+  # dark_gray = "#a89984";
+  # dark_orange = "#d65d0e";
 in {
   programs.qutebrowser = {
     enable = true;
+    loadAutoconfig = false;
+
+    aliases = {
+      q = "quit";
+      w = "session-save";
+      wq = "quit --save";
+    };
+
+    keyBindings = {
+      normal = {
+        "M" = "hint links spawn mpv {hint-url}";
+        "Z" = "hint links spawn st -e youtube-dl {hint-url}";
+        "t" = "set-cmd-text -s :open -t";
+        "xb" = "config-cycle statusbar.show always never";
+        "xt" = "config-cycle tabs.show always never";
+        "xx" = "config-cycle statusbar.show always never;; config-cycle tabs.show always never";
+      };
+    };
+
+    # Domain-specific settings must be done via extraConfig as they require 3 arguments
+    extraConfig = ''
+      config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
+      config.set('content.cookies.accept', 'all', 'devtools://*')
+      config.set('content.images', True, 'chrome-devtools://*')
+      config.set('content.images', True, 'devtools://*')
+      config.set('content.javascript.enabled', True, 'chrome-devtools://*')
+      config.set('content.javascript.enabled', True, 'devtools://*')
+      config.set('content.javascript.enabled', True, 'chrome://*/*')
+      config.set('content.javascript.enabled', True, 'qute://*/*')
+    '';
 
     searchEngines = {
       DEFAULT = "https://sx.dynn.xyz/search?q={}";
@@ -49,54 +80,78 @@ in {
 
     quickmarks = {
       # Uni
-      beecampus      = "https://beecampus.intec.edu.do/";
-      campusvirtual  = "https://campusvirtual.intec.edu.do/";
+      beecampus = "https://beecampus.intec.edu.do/";
+      campusvirtual = "https://campusvirtual.intec.edu.do/";
       # Mail
-      tutamail       = "https://app.tuta.com/mail";
-      protonmail     = "https://mail.proton.me/";
-      gmail          = "https://mail.google.com/";
-      outlook        = "https://outlook.live.com/";
+      tutamail = "https://app.tuta.com/mail";
+      protonmail = "https://mail.proton.me/";
+      gmail = "https://mail.google.com/";
+      outlook = "https://outlook.live.com/";
       # Social media
-      floss          = "https://floss.social/";
-      pixelfed       = "https://pixelfed.social/";
-      lemmy          = "https://lemmy.world/";
+      floss = "https://floss.social/";
+      pixelfed = "https://pixelfed.social/";
+      lemmy = "https://lemmy.world/";
       # Courses
-      edx            = "https://www.edx.org/";
-      khanacademy    = "https://khanacademy.org/";
-      udemy          = "https://www.udemy.com/";
-      coursera       = "https://www.coursera.org/";
+      edx = "https://www.edx.org/";
+      khanacademy = "https://khanacademy.org/";
+      udemy = "https://www.udemy.com/";
+      coursera = "https://www.coursera.org/";
       # Buying from the internet
-      eps            = "https://clientes.eps.com.do/";
-      vimenpaq       = "https://vimenpaq.com.do/";
-      amazon         = "https://www.amazon.com/";
-      ebay           = "https://ebay.com/";
+      eps = "https://clientes.eps.com.do/";
+      vimenpaq = "https://vimenpaq.com.do/";
+      amazon = "https://www.amazon.com/";
+      ebay = "https://ebay.com/";
       # Ebooks
-      annas          = "https://annas-archive.org/";
-      libgen         = "https://libgen.li/";
+      annas = "https://annas-archive.org/";
+      libgen = "https://libgen.li/";
       # Self hosted stuff
-      nextcloud      = "https://nc.dynn.xyz/";
-      vaultwarden    = "https://bw.dynn.xyz/";
-      kavita         = "https://kv.dynn.xyz/";
+      nextcloud = "https://nc.dynn.xyz/";
+      vaultwarden = "https://bw.dynn.xyz/";
+      kavita = "https://kv.dynn.xyz/";
       audiobookshelf = "https://ab.dynn.xyz/";
-      jellyfin       = "https://jf.dynn.xyz/";
-      navidrome      = "https://nd.dynn.xyz/";
-      aonsoku        = "https://ao.dynn.xyz/";
-      freshrss       = "https://rss.dynn.xyz/";
-      searxng        = "https://sx.dynn.xyz/";
-      portainer      = "https://pt.dynn.xyz/";
+      jellyfin = "https://jf.dynn.xyz/";
+      navidrome = "https://nd.dynn.xyz/";
+      aonsoku = "https://ao.dynn.xyz/";
+      freshrss = "https://rss.dynn.xyz/";
+      searxng = "https://sx.dynn.xyz/";
+      portainer = "https://pt.dynn.xyz/";
       # Admin portals for self hosted stuff
-      digitalocean   = "https://cloud.digitalocean.com/";
-      servarica      = "https://clients.servarica.com/";
-      dynadot        = "https://dynadot.com/";
+      digitalocean = "https://cloud.digitalocean.com/";
+      servarica = "https://clients.servarica.com/";
+      dynadot = "https://dynadot.com/";
       # Code forges
-      github         = "https://github.com/";
-      codeberg       = "https://codeberg.org/";
+      github = "https://github.com/";
+      codeberg = "https://codeberg.org/";
       # File shares
-      mega           = "https://mega.nz/";
-      gdrive         = "https://drive.google.com/drive/my-drive";
+      mega = "https://mega.nz/";
+      gdrive = "https://drive.google.com/drive/my-drive";
     };
 
     settings = {
+      tabs.show = "always";
+      downloads.location.directory = "~/Downloads";
+      url.default_page = "https://sx.dynn.xyz";
+      url.start_pages = "https://sx.dynn.xyz";
+      
+      colors.webpage.preferred_color_scheme = "dark";
+
+      content = {
+        notifications.enabled = false;
+        blocking.method = "both";
+        blocking.enabled = true;
+        autoplay = false;
+        register_protocol_handler = false;
+      };
+
+      fonts = {
+        default_family = "${theme.font}";
+        default_size = "${toString theme.font-size}pt";
+        completion.entry = "${toString theme.font-size}pt '${theme.font-mono}'";
+        debug_console = "${toString theme.font-size}pt '${theme.font-mono}'";
+        prompts = "default_size sans-serif";
+        statusbar = "${toString theme.font-size}pt '${theme.font}'";
+      };
+
       colors = {
         completion = {
           fg = [ fg1 bright_aqua bright_yellow ];

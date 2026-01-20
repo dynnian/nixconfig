523a593fceb6a5d196569fa7158a32ee4bc95841
Lian Dynn <lian_dynn@pm.me>
2026-01-20 00:39:39 -0400

    Readded pipewire configs

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -1,4 +1,4 @@
-{ hostname, lib, pkgs, ... }: {
+{ pkgs, ... }: {
   security.rtkit.enable = true;
 
   services.pipewire = {
@@ -13,7 +13,7 @@
     jack.enable = true;
     wireplumber.enable = true;
 
-    extraConfig = lib.mkIf (hostname == "zweihander") {
+    extraConfig = {
       pipewire."10-custom-formats.conf" = {
         "context.properties" = {
           "default.clock.allowed-rates" = [

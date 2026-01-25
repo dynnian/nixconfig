89f878dab2a51f05b80e48cdf47ad8545d8482e9
Lian Dynn <lian_dynn@pm.me>
2026-01-25 13:45:53 -0400

    Discord is broken

--- home/packages.nix
+++ home/packages.nix
@@ -55,4 +55,16 @@ in {
     prismlauncher
     lutris
   ];
+
+  xdg.desktopEntries.discord = {
+    name = "Discord";
+    genericName = "Internet Messenger";
+    comment = "All-in-one voice and text chat for gamers";
+    # The fix: wrap the command in sh and redirect output to /dev/null
+    exec = "sh -c \"discord %U > /dev/null 2>&1\"";
+    icon = "discord";
+    type = "Application";
+    categories = [ "Network" "InstantMessaging" ];
+    mimeType = [ "x-scheme-handler/discord" ];
+  };
 }

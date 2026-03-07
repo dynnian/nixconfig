dfd7ec236f8bad415bf6fe4bcc46d5f7f4df287f
Lian Dynn <lian_dynn@pm.me>
2026-03-07 19:29:27 -0400

    Mullvad 3

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,13 +1,14 @@
 { ... }: {
   imports = [
+    ./flatpak.nix
+    ./greetd.nix
+    ./gvfs.nix
+    ./mullvad.nix
     ./pipewire.nix
     ./power-profiles.nix
     ./printing.nix
     ./timesyncd.nix
     ./keyring.nix
-    ./greetd.nix
     ./udisks2.nix
-    ./flatpak.nix
-    ./gvfs.nix
   ];
 }

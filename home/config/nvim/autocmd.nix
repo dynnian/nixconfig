90c03d2c05db0d25dafee8c1f7ba78659f60bdb8
Lian Dynn <dynn@tuta.com>
2026-06-22 02:36:34 -0400

    yazi fixes and some nvim adjustments

--- home/config/nvim/autocmd.nix
+++ home/config/nvim/autocmd.nix
@@ -5,5 +5,30 @@
       pattern = [ "json" "nix" ];
       command = "setlocal tabstop=2 shiftwidth=2 softtabstop=2";
     }
+    {
+      event = [ "BufEnter" ];
+      pattern = [ "*" ];
+      callback = { __raw = ''
+        function()
+          -- Ignore special or non-file buffers
+          if vim.bo.buftype ~= "" or vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 0 then
+            return
+          end
+
+          -- Find the nearest directory containing a .git folder starting from the current file
+          local root = vim.fs.find({ '.git' }, { upward = true, path = vim.api.nvim_buf_get_name(0) })[1]
+          if root then
+            -- Set Neovim's global path to the folder containing the .git directory
+            vim.api.nvim_set_current_dir(vim.fs.dirname(root))
+          else
+            -- Fallback: If not in a git repo, just change directory to the file's folder
+            local current_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+            if vim.fn.isdirectory(current_dir) == 1 then
+              vim.api.nvim_set_current_dir(current_dir)
+            end
+          end
+        end
+      ''; };
+    }
   ];
 }

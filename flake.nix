697d474b8499dd8b6c64d23c796539ffd1d0541b
Lian Dynn <dynn@tuta.com>
2026-08-24 09:07:12 -0400

    No luck

--- flake.nix
+++ flake.nix
@@ -20,10 +20,6 @@
       url = "github:MattiaPun/SubTUI";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-06cb-009a-fingerprint-sensor = {
-      url = "github:ahbnr/nixos-06cb-009a-fingerprint-sensor?ref=26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { 
@@ -32,8 +28,7 @@
     nixpkgs-unstable, 
     disko, 
     home-manager, 
-    nixvim, 
-    nixos-06cb-009a-fingerprint-sensor,
+    nixvim,
     ... 
   }@inputs:
     let
@@ -68,7 +63,6 @@
           ./hosts/${name}/configuration.nix
           ./hosts/${name}/disko.nix
           home-manager.nixosModules.home-manager
-          nixos-06cb-009a-fingerprint-sensor.nixosModules."06cb-009a-fingerprint-sensor"
           ({ config, ... }: {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
--- hosts/claymore/hardware-optimizations.nix
+++ hosts/claymore/hardware-optimizations.nix
@@ -1,7 +1,3 @@
 { lib, ... }: {
   services.throttled.enable = lib.mkDefault true;
-  services."06cb-009a-fingerprint-sensor" = {                                 
-    enable = true;                                                            
-    backend = "python-validity";                                              
-  };   
 }

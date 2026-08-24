bf5bd831ca4871791dc7d4a4327dc4afc547bed8
Lian Dynn <dynn@tuta.com>
2026-08-24 01:17:44 -0400

    Trying to add netbird

--- modules/services/netbird.nix
+++ modules/services/netbird.nix
@@ -2,11 +2,5 @@
   services.netbird = {
     enable = true;
     ui.enable = true;
-    clients.wt0 = {
-      port = 51821;
-      ui.enable = true;
-      openFirewall = true;
-      openInternalFirewall = true;
-    };
   };
 }

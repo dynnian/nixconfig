f0d24101cd846dc54ec2ff42b686c4ad4f01c3c5
Lian Dynn <dynn@tuta.com>
2026-08-24 01:44:43 -0400

    Trying to add netbird

--- modules/services/netbird.nix
+++ modules/services/netbird.nix
@@ -2,7 +2,16 @@
   services.netbird = {
     enable = true;
     package = pkgs.unstable.netbird;
-    ui.enable = true;
-    ui.package = pkgs.unstable.netbird-ui;
+    ui = {
+      enable = true;
+      package = pkgs.unstable.netbird-ui.overrideAttrs (oldAttrs: {
+        postInstall = (oldAttrs.postInstall or "") + ''
+          # Ensure Exec=netbird-ui is present for the NixOS wrapper substitution
+          if [ -f $out/share/applications/netbird.desktop ]; then
+            sed -i 's|^Exec=.*|Exec=netbird-ui|' $out/share/applications/netbird.desktop
+          fi
+        '';
+      });
+    };
   };
 }

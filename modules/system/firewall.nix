1bbccccf7cc134948f9459598011c09b90085679
Lian Dynn <dynn@tuta.com>
2026-08-26 16:53:30 -0400

    Trying to fix netbird with firewall

--- modules/system/firewall.nix
+++ modules/system/firewall.nix
@@ -5,6 +5,11 @@
     allowedUDPPorts = [ ];
     allowPing = true;
     logRefusedConnections = true;
-    trustedInterfaces = [ "virbr0" ];
+    
+    # 1. Allow NetBird's WireGuard interface
+    trustedInterfaces = [ "virbr0" "wt0" ];
+
+    # 2. Prevent the kernel from dropping WireGuard packets
+    checkReversePath = "loose";
   };
 }

a05085a3cca24f9db1144f07c8ec85c44dac66ca
Lian Dynn <dynn@tuta.com>
2026-09-09 00:58:03 -0400

    Okay seems like the workaround isn't needed anymore is it

--- modules/services/netbird.nix
+++ modules/services/netbird.nix
@@ -4,14 +4,15 @@
     package = pkgs.unstable.netbird;
     ui = {
       enable = true;
-      package = pkgs.unstable.netbird-ui.overrideAttrs (oldAttrs: {
-        postInstall = (oldAttrs.postInstall or "") + ''
-          # Ensure Exec=netbird-ui is present for the NixOS wrapper substitution
-          if [ -f $out/share/applications/netbird.desktop ]; then
-            sed -i 's|^Exec=.*|Exec=netbird-ui|' $out/share/applications/netbird.desktop
-          fi
-        '';
-      });
+      package = pkgs.unstable.netbird-ui;
+#      package = pkgs.unstable.netbird-ui.overrideAttrs (oldAttrs: {
+#        postInstall = (oldAttrs.postInstall or "") + ''
+#          # Ensure Exec=netbird-ui is present for the NixOS wrapper substitution
+#          if [ -f $out/share/applications/netbird.desktop ]; then
+#            sed -i 's|^Exec=.*|Exec=netbird-ui|' $out/share/applications/netbird.desktop
+#          fi
+#        '';
+#      });
     };
   };
 }
